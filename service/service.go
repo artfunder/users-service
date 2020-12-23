5f50fcde56f097531aba52de6eba401471e3b138
Steven Kaufman <steven.kaufman0813@gmail.com>
2020-12-22 21:44:07 -0600

    changed UsersService from interface to direct implementation

--- service/service.go
+++ service/service.go
@@ -3,10 +3,29 @@ package service
 import "github.com/artfunder/structs"
 
 // UsersService ...
-type UsersService interface {
-	GetAll() ([]structs.User, error)
-	GetOne(id int) (structs.User, error)
-	Create(userInfo structs.User) (structs.User, error)
-	Update(id int, userInfo map[string]interface{}) (structs.User, error)
-	Delete(id int) (structs.User, error)
+type UsersService struct{}
+
+// GetAll ...
+func (UsersService) GetAll() ([]structs.User, error) {
+	return []structs.User{}, nil
+}
+
+// GetOne ...
+func (UsersService) GetOne(id int) (structs.User, error) {
+	return structs.User{}, nil
+}
+
+// Create ...
+func (UsersService) Create(userInfo structs.User) (structs.User, error) {
+	return structs.User{}, nil
+}
+
+// Update ...
+func (UsersService) Update(id int, userInfo map[string]interface{}) (structs.User, error) {
+	return structs.User{}, nil
+}
+
+// Delete ...
+func (UsersService) Delete(id int) (structs.User, error) {
+	return structs.User{}, nil
 }
